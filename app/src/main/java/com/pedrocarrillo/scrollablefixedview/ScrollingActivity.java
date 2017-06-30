69b9ea0bdf04557481fb1750dc90bf37788dbec8
Pedro Carrillo <carrillochero6@gmail.com>
2017-06-30 00:33:57 -0400

    first commit

---
+++ app/src/main/java/com/pedrocarrillo/scrollablefixedview/ScrollingActivity.java
@@ -0,0 +1,75 @@
+package com.pedrocarrillo.scrollablefixedview;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.NestedScrollView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ScrollingActivity extends AppCompatActivity {
+
+    NestedScrollView nestedScrollView;
+    View stackedView;
+    View innerView;
+    int threshold = 30;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        nestedScrollView = (NestedScrollView) findViewById(R.id.nested);
+        stackedView = findViewById(R.id.stacked_view);
+        innerView = findViewById(R.id.innerView);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        nestedScrollView.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+                int[] location = {0,0};
+                innerView.getLocationOnScreen(location);
+                if (location[1] - stackedView.getY() < stackedView.getMeasuredHeight() + threshold) {
+                    stackedView.setVisibility(View.INVISIBLE);
+                } else {
+                    stackedView.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
