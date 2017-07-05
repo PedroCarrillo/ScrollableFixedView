a42e2346d02afdcfe93787637ab7eeba52bc5b02
Pedro Carrillo <carrillochero6@gmail.com>
2017-07-05 00:05:25 -0400

    reducing the number of views inflated

--- app/src/main/java/com/pedrocarrillo/scrollablefixedview/ScrollingActivity.java
+++ app/src/main/java/com/pedrocarrillo/scrollablefixedview/ScrollingActivity.java
@@ -1,10 +1,12 @@
 package com.pedrocarrillo.scrollablefixedview;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.NestedScrollView;
+import android.support.v4.widget.Space;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
@@ -12,13 +14,15 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewTreeObserver;
 
 public class ScrollingActivity extends AppCompatActivity {
 
     NestedScrollView nestedScrollView;
     View stackedView;
-    View innerView;
-    int threshold = 30;
+    Space placeholder;
+    int threshold = 60;
+    float initialPosition = 0.0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,28 +32,50 @@ public class ScrollingActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         nestedScrollView = (NestedScrollView) findViewById(R.id.nested);
         stackedView = findViewById(R.id.stacked_view);
-        innerView = findViewById(R.id.innerView);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+
+        placeholder = (Space) findViewById(R.id.placeholder);
+        final Rect scrollBounds = new Rect();
+        nestedScrollView.getHitRect(scrollBounds);
+
+        stackedView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        nestedScrollView.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
-            @Override
-            public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
-                int[] location = {0,0};
-                innerView.getLocationOnScreen(location);
-                if (location[1] - stackedView.getY() < stackedView.getMeasuredHeight() + threshold) {
-                    stackedView.setVisibility(View.INVISIBLE);
-                } else {
-                    stackedView.setVisibility(View.VISIBLE);
+            public void onGlobalLayout() {
+                if (initialPosition == 0f) {
+                    initialPosition = stackedView.getY();
+                    nestedScrollView.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
+                        @Override
+                        public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+                            final int[] location = {0, 0};
+                            placeholder.getLocationOnScreen(location);
+                            // Check if the placeholder is visible in the scroll
+                            if (placeholder.getLocalVisibleRect(scrollBounds)) {
+                                // if it's visible and our view is hidden, we should show it
+                                if (stackedView.getVisibility() == View.GONE) {
+                                    stackedView.setVisibility(View.VISIBLE);
+                                }
+                                // updating the views Y according to the placeholders Y
+                                stackedView.setY(location[1] - stackedView.getHeight() - (threshold));
+                            } else {
+                                // If the placeholder it's not visible, we should decide whether we want to
+                                // hide or not the view. In this case if the view comes from the bottom we hide it. (scrollBounds.bottom > 0)
+                                if (location[1] > scrollBounds.bottom && scrollBounds.bottom > 0) {
+                                    stackedView.setVisibility(View.VISIBLE);
+                                    stackedView.setY(initialPosition - stackedView.getHeight());
+                                } else {
+                                    stackedView.setVisibility(View.GONE);
+                                }
+                            }
+                        }
+                    });
                 }
             }
         });
+     }
 
+    @Override
+    public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
+        return super.onCreateView(parent, name, context, attrs);
     }
 
     @Override
